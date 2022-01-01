041acb19c1f8ce891f39568872cccd877892750a
Katsuya Iida <katsuya.iida@gmail.com>
2022-01-01 12:45:25 +0900

    Target .NET Core 3.1 SDK (#5)

--- NeMoOnnxSharp/Program.cs
+++ NeMoOnnxSharp/Program.cs
@@ -1,44 +1,54 @@
-﻿using NeMoOnnxSharp;
+﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 
-short[] ReadWav(string waveFile)
+namespace NeMoOnnxSharp
 {
-    using var stream = File.OpenRead(waveFile);
-    using var reader = new BinaryReader(stream);
-    string fourCC = new string(reader.ReadChars(4));
-    if (fourCC != "RIFF")
-        throw new InvalidDataException();
-    int chunkLen = reader.ReadInt32();
-    fourCC = new string(reader.ReadChars(4));
-    if (fourCC != "WAVE")
-        throw new InvalidDataException();
-    while (true)
+    internal class Program
     {
-        fourCC = new string(reader.ReadChars(4));
-        chunkLen = reader.ReadInt32();
-        byte[] byteData = reader.ReadBytes(chunkLen);
-        if (fourCC == "data")
+        static void Main(string[] args)
         {
-            return MemoryMarshal.Cast<byte, short>(byteData).ToArray();
+            string appDirPath = AppDomain.CurrentDomain.BaseDirectory;
+            string modelPath = Path.Combine(appDirPath, "QuartzNet15x5Base-En.onnx");
+            string inputDirPath = Path.Combine(appDirPath, "..", "..", "..", "..", "test_data");
+            string inputPath = Path.Combine(inputDirPath, "transcript.txt");
+
+            using var recognizer = new SpeechRecognizer(modelPath);
+            using var reader = File.OpenText(inputPath);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] parts = line.Split("|");
+                string name = parts[0];
+                string targetText = parts[1];
+                string waveFile = Path.Combine(inputDirPath, name);
+                var waveform = ReadWav(waveFile);
+                string predictText = recognizer.Recognize(waveform);
+                Console.WriteLine("{0}|{1}|{2}", name, targetText, predictText);
+            }
+        }
+
+        static short[] ReadWav(string waveFile)
+        {
+            using var stream = File.OpenRead(waveFile);
+            using var reader = new BinaryReader(stream);
+            string fourCC = new string(reader.ReadChars(4));
+            if (fourCC != "RIFF")
+                throw new InvalidDataException();
+            int chunkLen = reader.ReadInt32();
+            fourCC = new string(reader.ReadChars(4));
+            if (fourCC != "WAVE")
+                throw new InvalidDataException();
+            while (true)
+            {
+                fourCC = new string(reader.ReadChars(4));
+                chunkLen = reader.ReadInt32();
+                byte[] byteData = reader.ReadBytes(chunkLen);
+                if (fourCC == "data")
+                {
+                    return MemoryMarshal.Cast<byte, short>(byteData).ToArray();
+                }
+            }
         }
     }
-}
-
-string appDirPath = AppDomain.CurrentDomain.BaseDirectory;
-string modelPath = Path.Combine(appDirPath, "QuartzNet15x5Base-En.onnx");
-string inputDirPath = @"..\..\..\..\test_data";
-string inputPath = Path.Combine(inputDirPath, "transcript.txt");
-
-using var recognizer = new SpeechRecognizer(modelPath);
-using var reader = File.OpenText(inputPath);
-string line;
-while ((line = reader.ReadLine()) != null)
-{
-    string[] parts = line.Split("|");
-    string name = parts[0];
-    string targetText = parts[1];
-    string waveFile = Path.Combine(inputDirPath, name);
-    var waveform = ReadWav(waveFile);
-    string predictText = recognizer.Recognize(waveform);
-    Console.WriteLine("{0}|{1}|{2}", name, targetText, predictText);
 }
